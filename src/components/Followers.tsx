86eadd9cd5eb0b63966acc08c6b3967f93c01fd0
calvin <puram.calvin@gmail.com>
2020-07-18 23:39:49 +0100

    completed the user page

--- src/components/Followers.tsx
+++ src/components/Followers.tsx
@@ -1,11 +1,25 @@
-import React from "react";
-// import { GithubContext } from "../context/context";
+import React, { useContext } from "react";
+import { GithubContext } from "../context/context";
 import styled from "styled-components";
 
 const Followers = () => {
+  const { githubFollowers } = useContext(GithubContext);
   return (
     <Wrapper>
-      <h1>followers</h1>
+      <div className='followers'>
+        {githubFollowers.map((follower, index) => {
+          const { avatar_url: img, html_url, login } = follower;
+          return (
+            <article key={index}>
+              <img src={img} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>{html_url}</a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
     </Wrapper>
   );
 };
